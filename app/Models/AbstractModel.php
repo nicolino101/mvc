af1f77ab7b7816ebbe4ad42ac6e8653beb6ad25c
Nicolino101 <nicolino101@gmail.com>
2018-11-27 00:32:36 -0800

    better

--- app/Models/AbstractModel.php
+++ app/Models/AbstractModel.php
@@ -2,7 +2,51 @@
 namespace App\Models;
 
 class AbstractModel {
-    public function __call($method, $args){
+    protected $db;
+    protected static $dbtype = 'sqlite';
+    
+    //chown -R www-data:www-data /var/www/deploy/db
+    //LIVE: sqlite:/var/www/deploy/db/db1.sq3
+    protected function conn(){
+        switch(static::$dbtype){
+            case "sqlite3":
+                $dns = "sqlite:memory";
+                $this->db = new \PDO($dns);
+                break;
+            case "mysql":
+                $host = 'localhost';
+                $dbname = 'testing';
+                $user = 'root';
+                $pass = '';
+                $this->db = new \PDO('mysql:host=localhost;dbname=testing', $user);
+                break;
+            default:
+                $dns = "sqlite:memory";
+                $this->db = new \PDO($dns);
+                break;
+        }
+        $this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    }
+    
+    // sets the db on the fly mysql || sqlite3
+    public function setDbType($type = 'sqlite3'){
+        static::$dbtype = $type;
+    }
+    
+    public function getDBType(){
+        return static::$dbtype;
+    }
+    
+    protected function execute(string $sql, array $params = null){
+        $stmt = $this->db->prepare($sql);
+        if($stmt){
+            $stmt->execute($params);
+            return $stmt->fetchAll(\PDO::FETCH_OBJ);
+        }else{
+            return false;
+        }
+    }
+    public function __call($method, $args) {
         if(preg_match('/set/', $method)){
             $m = lcfirst(str_replace('set', '', $method));
             if(property_exists($this, $m)){
